42da04c9ebddf16238bdbc26884c4df3a77ab44f
Briheet Singh Yadav <briheetyadav@gmail.com>
2026-09-09 23:44:16 +0530

    upd: yazi upd

--- modules/common/yazi.nix
+++ modules/common/yazi.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.yazi = {
     enable = true;
@@ -6,13 +6,51 @@
     enableNushellIntegration = true;
     shellWrapperName = "y";
 
-    settings.opener.edit = [
+    extraPackages = [ pkgs.glow ];
+
+    flavors.nord = pkgs.yaziPlugins.nord;
+    theme.flavor = {
+      dark = "nord";
+      light = "nord";
+    };
+
+    plugins = {
+      piper = pkgs.yaziPlugins.piper;
+      toggle-pane = pkgs.yaziPlugins.toggle-pane;
+    };
+
+    keymap.mgr.prepend_keymap = [
       {
-        run = "hx %s";
-        desc = "Helix";
-        block = true;
-        for = "unix";
+        on = "T";
+        run = "plugin toggle-pane max-preview";
+        desc = "Maximize or restore preview";
       }
     ];
+
+    settings = {
+      # Parent directory, file list, preview: give previews half the window.
+      mgr.ratio = [ 1 3 4 ];
+      preview = {
+        wrap = "yes";
+        tab_size = 2;
+      };
+
+      # Keep the built-in code/media previewers; render Markdown with Glow.
+      plugin.prepend_previewers = [
+        {
+          url = "*.md";
+          run = ''piper -- CLICOLOR_FORCE=1 glow -w=$w -s=dark "$1"'';
+        }
+      ];
+
+      opener.edit = [
+        {
+          run = "hx %s";
+          desc = "Helix";
+          block = true;
+          for = "unix";
+        }
+      ];
+    };
   };
 }
